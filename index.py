8fbe293e1c9b1a8d71507c84ac310d03884fc0e0
jananga99 <jananga1999@gmail.com>
2024-04-04 16:12:36 +0530

    Functionalities added

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import random
+from bson import ObjectId
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from enum import Enum
@@ -13,9 +14,39 @@ app = Flask(__name__)
 
 # Connect to MongoDB
 db_url = os.getenv("DB_URL")
+db = os.getenv("DB")
+print(db_url)
 client = MongoClient(db_url)
-db = client['global']
-chunkdata = db['chunkdata']
+db = client[db]
+chunk_data_table = db['chunkdata']
+
+@app.route('/chunk', methods=['POST'])
+def upload_chunk():
+    chunk_id = request.form['id']
+    chunk = request.form['chunk']
+    next_chunk_id = request.form['next_chunk_id']
+    next_chunk_node_id = request.form['next_chunk_node_id']
+    save_chunk({
+        '_id': chunk_id,
+        'chunk': chunk,
+        'next_chunk_id': next_chunk_id,
+        'next_chunk_node_id': next_chunk_node_id,
+        'created_at': datetime.now()
+    })
+    return jsonify({'success': True, 'process': 'Chunk uploaded successfully.'})
+
+@app.route('/chunk/<chunk_id>', methods=['GET'])
+def get_chunk_by_id(chunk_id):
+    chunk = get_chunk(chunk_id)
+    if chunk:
+        return jsonify({'success': True, 'data': chunk})
+    return jsonify({'success': False, 'message': 'Chunk not found.'})
+
+def save_chunk(chunk_data):
+    chunk_data_table.insert_one(chunk_data)
+
+def get_chunk(chunk_id):
+    return chunk_data_table.find_one({'_id': chunk_id})
 
 if __name__ == '__main__':
-    app.run(debug=True,port=int(os.getenv('PORT', 5000)))  # Run the Flask app in debug mode
+    app.run(debug=True,port=int(os.getenv('PORT', 6000)))  # Run the Flask app in debug mode
